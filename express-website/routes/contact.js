d3f6acbc47480ace5653c54a589a3a0ac277fd0a
imajeet <ajeetbpr@gmail.com>
2018-05-26 18:05:57 +0800

    ExpressJS Website with Nodemailer Setup

---
+++ express-website/routes/contact.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+router.post('/send', function(req, res, next){
+     var transporter = nodemailer.createTransport({
+         service: 'Gmail',
+         auth: {
+         	user: 'ajeetbpr@gmail.com',
+         	pass: 'something'
+         }
+     });
+     var mailOptions = { 
+         from: 'Ajeet Singh <ajeetbpr@hotmail.com>',
+         to: 'ajeetbpr@gmail.com',
+         subject: 'Website Submission',
+         text: 'You have a new submission with following details...Name: '+req.body.name+' Email: '+req.body.email+' Message: '+req.body.message,
+         html: '<p>You have a new submission with following details...</p><ul><li>Name: '+req.body.name+' </li><li>Email: '+req.body.email+' </li><li>Message: '+req.body.message+' </li></ul>'
+};
+transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+      	console.log(error);
+      	res.redirect('/');
+      }else{
+      	console.log('Message Sent' +info.response);
+      	res.redirect('/');
+      }
+});
+});
+
+module.exports = router;
